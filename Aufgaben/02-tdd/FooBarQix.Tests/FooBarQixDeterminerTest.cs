07a9daafd2bebc996c7bd64b2877de71aa213893
Simon-Hodel <simon-hodel@gmx.ch>
2022-11-09 12:11:30 +0100

    Aufgabe 2

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -2,11 +2,101 @@ using Xunit;
 
 namespace FooBarQix.Tests
 {
-    public class FooBarQixDeterminerTest
+  public class FooBarQixDeterminerTest
+  {
+    [Fact]
+    public void Determine_ReturnsNumberAsString()
     {
-        [Fact]
-        public void Test1()
-        {
-        }
+      int number = 8;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal(number.ToString(), result);
+
     }
+    [Fact]
+    public void Determine_ReturnsFoo()
+    {
+      var number = 3;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("Foo", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsBar()
+    {
+      int number = 5;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("Bar", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsQix()
+    {
+      int number = 7;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("Qix", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsFooBar()
+    {
+      int number = 15;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("FooBar", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsFooQix()
+    {
+      int number = 21;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("FooQix", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsFooBarQix()
+    {
+      int number = 105;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("FooBarQix", result);
+
+    }
+
+    [Fact]
+    public void Determine_ReturnsBarQix()
+    {
+      int number = 35;
+      var testee = new FooBarQixDeterminer();
+
+      var result = testee.Determine(number);
+
+      Assert.Equal("BarQix", result);
+
+    }
+  }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -1,7 +1,30 @@
 ﻿namespace FooBarQix
 {
-    public class FooBarQixDeterminer
+  public class FooBarQixDeterminer
+  {
+    string Returnstring;
+    public string Determine(int input)
     {
 
+
+      if (input % 3 == 0)
+      {
+        Returnstring += "Foo";
+      }
+      if (input % 5 == 0)
+      {
+        Returnstring += "Bar";
+      }
+      if (input % 7 == 0)
+      {
+        Returnstring += "Qix";
+      }
+      if (input % 3 != 0 && input % 5 != 0 && input % 7 != 0)
+      {
+        return input.ToString();
+      }
+
+      return Returnstring;
     }
+  }
 }
